5247162de4d840a0e659ac2cf2c71d7be844cce6
neo_elon <parkyangkyu@gmail.com>
2026-06-14 12:24:59 +0900

    별점

--- app.js
+++ app.js
@@ -371,12 +371,8 @@ function renderGallery() {
 
     const sentence = book.sentence
       ? `<div class="ov-sentence">${esc(book.sentence)}</div>` : '';
-    const ratingBadge = book.rating
-      ? `<div class="card-rating-badge">${'★'.repeat(book.rating)}</div>` : '';
-
     card.innerHTML = `
       ${imgPart}
-      ${ratingBadge}
       <div class="book-hover-overlay">
         <div class="ov-title">${esc(book.title)}</div>
         <div class="ov-author">${esc(book.author||'')}</div>
