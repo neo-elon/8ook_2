f5967e1ba006f3652ba7aef2ed37f09c98b4b5aa
neo_elon <parkyangkyu@gmail.com>
2026-06-14 12:03:16 +0900

    필요옶는 파일 삭제

--- read_saved_json.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const recordMap = JSON.parse(fs.readFileSync('recordMap.json', 'utf8'));
-
-const blockKeys = Object.keys(recordMap.block);
-console.log('First block ID:', blockKeys[0]);
-console.log('First block raw:', JSON.stringify(recordMap.block[blockKeys[0]], null, 2));
